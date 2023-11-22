4b6ca4ab0556fd6ec3538367361dc9380ab0afb8
pdoshi <pr882442@dal.ca>
2023-11-22 15:45:42 -0400

    navbar tried,will complete later

---
+++ demo-web/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+export default function Navbar(props) {
+    return (
+        <div className="navbar">
+            <div className="hamburger"> hR</div>
+          
+            <div className="logo">logo</div>
+          
+            <div className="search"><img src="../../public/searchbar.png" alt="searchbar"></img> </div>
+           
+            <div className="shop">s</div>
+          
+            
+        </div>
+    )
+}
