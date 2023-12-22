175643ab065f716f2b58bf9ad12fa67677fb21ff
pdoshi <pr882442@dal.ca>
2023-12-22 16:01:41 -0400

    Structure  of Navbar complete

--- demo-web/src/components/Navbar.js
+++ demo-web/src/components/Navbar.js
@@ -1,25 +1,42 @@
 import React, { useState } from 'react'
-import { FaHome, FaSearch, FaShoppingCart } from 'react-icons/fa';
+import { FaHome, FaShoppingCart } from 'react-icons/fa';
 import './../App.css';
+import ReactSwitch from 'react-switch'
+
 const Navbar= () =>{
 
     const [menuOpen, setMenu] = useState(false);
-
     const toggleMenu = () =>{
         setMenu(!menuOpen);
     }
-    const [searchOpen, setSearch] = useState(false);
 
+    const [searchOpen, setSearch] = useState(false);
     const toggleSearch = () =>{
         setSearch(!searchOpen);
+    }
+  
+    const [mode, setMode] = useState('light');
+    const toggleMode = ()=>{
+      if(mode === 'light'){
+        setMode('dark');
+        document.body.style.backgroundColor = '#042743';
+        document.body.style.color = 'white';
+      }
+      else{
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+      }
     }
 
 return(
     <nav className='navbar'>
-        <div className="hamburger-menu" onMouseEnter={toggleMenu} onMouseLeave={toggleMenu}>
+      <div className="hamburger-menu" onMouseEnter={toggleMenu} onMouseLeave={toggleMenu}>
         <div className="hamburger-icon">&#9776;</div>
+      </div>
         {menuOpen && (
-          <ul className="menu-items">
+          <div className='menu-container'onMouseLeave={toggleMenu}>
+          <ul className="menu-items" >
             <li href="#home">
               <FaHome /> 
             </li>
@@ -27,8 +44,9 @@ return(
             <li href="#subject2">Subject 2</li>
     
           </ul>
+          </div>
         )}
-      </div>
+      
       <div className="logo">
         <a href="#home">Logo</a>
       </div>
@@ -37,9 +55,6 @@ return(
         {searchOpen && (
         <>
           <input type="text" placeholder="Search..." />
-          <button>
-            <FaSearch />
-          </button>
         </>
         )}
       </div>
@@ -48,6 +63,29 @@ return(
         <a href="#cart">
           <FaShoppingCart />
         </a>
+      </div>
+      <div className="login">
+        <a href="#login">
+          Log In
+        </a>
+      </div>
+      <div className="signup">
+        <a href="#signup">
+          Sign Up
+        </a>
+      </div>
+      <div className="darkmode" onClick={toggleMode}> 
+        <label htmlFor="darkmode-switch" className="darkmode-label">
+          Dark Mode
+        </label>
+        <ReactSwitch
+         checked={mode === 'dark'}
+         onChange={toggleMode}
+         onColor="#2E2E2E"
+         offColor="#D3D3D3"
+         uncheckedIcon={false}
+         checkedIcon={false}
+        />
       </div>
     </nav>
   );
